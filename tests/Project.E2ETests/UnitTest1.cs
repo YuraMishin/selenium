adf50ded5123d9f88f549d448cf26b582111110b
Mishin Yura <mishin.inbox@gmail.com>
2021-04-18 12:35:05 +0300

    Located web elements with XPath. Closes #4

--- tests/Project.E2ETests/UnitTest1.cs
+++ tests/Project.E2ETests/UnitTest1.cs
@@ -9,6 +9,8 @@ namespace Project.E2ETests
 {
   public class UnitTest1 : IDisposable
   {
+    #region Bootstrap
+
     public readonly IWebDriver Driver;
     public UnitTest1()
     {
@@ -21,6 +23,54 @@ namespace Project.E2ETests
       // Tear down (called once per test)
       Driver.Quit();
     }
+
+    private static IWebDriver CreateWebDriver()
+    {
+      string driverDirectory = System.IO.Path.GetDirectoryName(typeof(UnitTest1).Assembly.Location) ?? ".";
+      bool isDebuggerAttached = System.Diagnostics.Debugger.IsAttached;
+
+      return CreateChromeDriver(driverDirectory, isDebuggerAttached);
+    }
+
+    private static IWebDriver CreateChromeDriver(
+      string driverDirectory,
+      bool isDebuggerAttached)
+    {
+      var options = new ChromeOptions();
+
+      if (!isDebuggerAttached)
+      {
+        //options.AddArgument("--headless");
+      }
+
+      // HACK Workaround for "(unknown error: DevToolsActivePort file doesn't exist)"
+      if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+      {
+        options.AddArgument("--no-sandbox");
+      }
+
+      return new ChromeDriver(driverDirectory, options);
+    }
+
+    private void HighlightElementUsingJavaScript(By locationStrategy, int duration = 2)
+    {
+      var element = Driver.FindElement(locationStrategy);
+      var originalStyle = element.GetAttribute("style");
+      IJavaScriptExecutor JavaScriptExecutor = Driver as IJavaScriptExecutor;
+      JavaScriptExecutor.ExecuteScript("arguments[0].setAttribute(arguments[1], arguments[2])",
+        element,
+        "style",
+        "border: 7px solid yellow; border-style: dashed;");
+
+      if (duration <= 0) return;
+      Thread.Sleep(TimeSpan.FromSeconds(duration));
+      JavaScriptExecutor.ExecuteScript("arguments[0].setAttribute(arguments[1], arguments[2])",
+        element,
+        "style",
+        originalStyle);
+    }
+
+    #endregion
 
     [Fact]
     public void Method_Returns_When()
@@ -69,51 +119,10 @@ namespace Project.E2ETests
 
     }
 
-    private static IWebDriver CreateWebDriver()
+    [Fact]
+    public void XPathLocation()
     {
-      string driverDirectory = System.IO.Path.GetDirectoryName(typeof(UnitTest1).Assembly.Location) ?? ".";
-      bool isDebuggerAttached = System.Diagnostics.Debugger.IsAttached;
 
-      return CreateChromeDriver(driverDirectory, isDebuggerAttached);
-    }
-
-    private static IWebDriver CreateChromeDriver(
-        string driverDirectory,
-        bool isDebuggerAttached)
-    {
-      var options = new ChromeOptions();
-
-      if (!isDebuggerAttached)
-      {
-        //options.AddArgument("--headless");
-      }
-
-      // HACK Workaround for "(unknown error: DevToolsActivePort file doesn't exist)"
-      if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
-      {
-        options.AddArgument("--no-sandbox");
-      }
-
-      return new ChromeDriver(driverDirectory, options);
-    }
-
-
-    private void HighlightElementUsingJavaScript(By locationStrategy, int duration = 2)
-    {
-      var element = Driver.FindElement(locationStrategy);
-      var originalStyle = element.GetAttribute("style");
-      IJavaScriptExecutor JavaScriptExecutor = Driver as IJavaScriptExecutor;
-      JavaScriptExecutor.ExecuteScript("arguments[0].setAttribute(arguments[1], arguments[2])",
-          element,
-          "style",
-          "border: 7px solid yellow; border-style: dashed;");
-
-      if (duration <= 0) return;
-      Thread.Sleep(TimeSpan.FromSeconds(duration));
-      JavaScriptExecutor.ExecuteScript("arguments[0].setAttribute(arguments[1], arguments[2])",
-          element,
-          "style",
-          originalStyle);
     }
   }
 }
